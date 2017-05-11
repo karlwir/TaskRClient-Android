02931b1da23e1042cb644f5e61468b94faf0ca12
Karl Wirfelt <karl.wirfelt@gmail.com>
2017-05-11 13:15:54 +0200

    Added dummy home activity

---
+++ app/src/main/java/taskr/se/taskr/home/HomeActivity.java
@@ -0,0 +1,37 @@
+package taskr.se.taskr.home;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import taskr.se.taskr.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    /**
+     * Lists workitems in recycle view
+     * Updates list content on select from top tabs or swipe left right
+     * Creates intent to open work item detail activity
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
