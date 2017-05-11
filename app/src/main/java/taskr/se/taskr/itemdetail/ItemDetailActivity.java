753bc5164f07bd674d5c3caf8099b4b404800a7b
Kevin <Kevin.briffa@appman.se>
2017-05-11 13:27:25 +0200

    added Workitem Detail activity and fragment

---
+++ app/src/main/java/taskr/se/taskr/itemdetail/ItemDetailActivity.java
@@ -0,0 +1,15 @@
+package taskr.se.taskr.itemdetail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import taskr.se.taskr.R;
+
+public class ItemDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_detail);
+    }
+}
---
+++ app/src/main/java/taskr/se/taskr/itemdetail/ItemDetailFragment.java
@@ -0,0 +1,10 @@
+package taskr.se.taskr.itemdetail;
+
+/**
+ * Created by Kevin on 2017-05-11.
+ */
+
+public class ItemDetailFragment {
+
+
+}
