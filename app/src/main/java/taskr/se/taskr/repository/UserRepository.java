cd9fa42fc4aa79e33ee551479a072c3cd0b2eda7
Karl Wirfelt <karl.wirfelt@gmail.com>
2017-05-11 12:58:49 +0200

    Added repository dummy interfaces

---
+++ app/src/main/java/taskr/se/taskr/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package taskr.se.taskr.repository;
+
+/**
+ * Created by kawi01 on 2017-05-11.
+ */
+
+public interface UserRepository {
+    // List<User> getUsers();
+    // User getUser(String itemKey);
+    // User addOrUpdateUser(User user);
+    // void deleteUser(String itemKey);
+}
---
+++ app/src/main/java/taskr/se/taskr/repository/WorkItemRepository.java
@@ -0,0 +1,14 @@
+package taskr.se.taskr.repository;
+
+import java.util.List;
+
+/**
+ * Created by kawi01 on 2017-05-11.
+ */
+
+public interface WorkItemRepository {
+    // List<WorkItem> getWorkItems();
+    // WorkItem getWorkItem(String itemKey);
+    // WorkItem addOrUpdateWorkItem(WorkItem workItem);
+    // void deleteWorkItem(String itemKey);
+}
