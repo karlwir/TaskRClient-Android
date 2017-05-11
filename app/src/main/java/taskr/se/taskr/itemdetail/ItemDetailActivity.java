b74ac02f847c4a63d7d10acce845fd288266c9dd
Kevin <Kevin.briffa@appman.se>
2017-05-11 15:43:29 +0200

    added Workitem Detail activity and fragment

--- app/src/main/java/taskr/se/taskr/itemdetail/ItemDetailActivity.java
+++ app/src/main/java/taskr/se/taskr/itemdetail/ItemDetailActivity.java
@@ -1,5 +1,7 @@
 package taskr.se.taskr.itemdetail;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +9,16 @@ import taskr.se.taskr.R;
 
 public class ItemDetailActivity extends AppCompatActivity {
 
+    public static Intent createIntent(Context context){
+        return new Intent(context, ItemDetailActivity.class);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_detail);
+
+
+
     }
 }
--- app/src/main/java/taskr/se/taskr/itemdetail/ItemDetailFragment.java
+++ app/src/main/java/taskr/se/taskr/itemdetail/ItemDetailFragment.java
@@ -1,10 +1,21 @@
 package taskr.se.taskr.itemdetail;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by Kevin on 2017-05-11.
  */
 
-public class ItemDetailFragment {
+public class ItemDetailFragment extends Fragment{
 
-
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
