ee7143468edd004e61d72b27618680b8947eaa3f
Karl Wirfelt <karl.wirfelt@gmail.com>
2017-05-12 10:21:30 +0200

    Updated work item model for sqlite

--- app/src/main/java/taskr/se/taskr/model/WorkItem.java
+++ app/src/main/java/taskr/se/taskr/model/WorkItem.java
@@ -5,12 +5,26 @@ package taskr.se.taskr.model;
  */
 
 public class WorkItem {
+    private static final long DEFAULT_ID = -1;
+    private static final String DEFAULT_ITEMKEY = null;
 
+    private final long id;
+    private final String itemKey;
     private final String title;
     private final String description;
     private String status;
 
+    public WorkItem(long id, String itemKey, String title, String description, String status) {
+        this.id = id;
+        this.itemKey = itemKey;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+
     public WorkItem(String title, String description, String status) {
+        this.id = DEFAULT_ID;
+        this.itemKey = DEFAULT_ITEMKEY;
         this.title = title;
         this.description = description;
         this.status = status;
