eec8058244edba45152403f399340e4544a52fa0
Karl Wirfelt <karl.wirfelt@gmail.com>
2017-05-15 11:45:42 +0200

    Updated WorkItem.java and User.java

--- app/src/main/java/taskr/se/taskr/model/User.java
+++ app/src/main/java/taskr/se/taskr/model/User.java
@@ -1,19 +1,38 @@
 package taskr.se.taskr.model;
 
+import java.util.Set;
+
 /**
  * Created by Kevin on 2017-05-11.
  */
 
 public class User {
 
+    private static final long DEFAULT_ID = -1;
+    private static final String DEFAULT_ITEMKEY = null;
+
+    private final long id;
+    private final String itemKey;
     private final String firstname;
     private final String lastname;
     private final String username;
+    private Set<WorkItem> workItems;
 
-    public User(String firstname, String lastname, String username) {
+    public User(long id, String itemKey, String firstname, String lastname, String username, Set<WorkItem> workItems) {
+        this.id = id;
+        this.itemKey = itemKey;
         this.firstname = firstname;
         this.lastname = lastname;
         this.username = username;
+        this.workItems = workItems;
+    }
+
+    public User(String firstname, String lastname, String username, Set<WorkItem> workItems) {
+        this(DEFAULT_ID, DEFAULT_ITEMKEY, firstname, lastname, username, workItems);
+    }
+
+    public User(String firstname, String lastname, String username) {
+        this(DEFAULT_ID, DEFAULT_ITEMKEY, firstname, lastname ,username, null);
     }
 
     public String getUsername() {
@@ -26,5 +45,17 @@ public class User {
 
     public String getLastname() {
         return lastname;
+    }
+
+    public String getItemKey() {
+        return itemKey;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public boolean hasBeenPersisted() {
+        return id != DEFAULT_ID;
     }
 }
