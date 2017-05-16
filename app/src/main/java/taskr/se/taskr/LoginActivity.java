6f0eac377438e61a0a1be69db53144d6532a2b03
den4is <d.piric@hotmail.com>
2017-05-16 10:08:28 +0200

    Lagt till login view och nästa steg efter att man har klickat på knappen

--- app/src/main/java/taskr/se/taskr/LoginActivity.java
+++ app/src/main/java/taskr/se/taskr/LoginActivity.java
@@ -2,6 +2,8 @@ package taskr.se.taskr;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 import taskr.se.taskr.home.HomeActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -10,7 +12,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+    }
 
+
+    public void loginButton(View view) {
         startActivity(HomeActivity.createIntent(this));
     }
 }
+
